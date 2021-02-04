6494b582da7f430812316dcaa49cfeb820faef5b
Khoi Van <33493502+khoivan88@users.noreply.github.com>
2021-02-04 12:21:49 -0500

    Feat: turn off 'HTTPCACHE' so that the result is always fresh

--- src/list_jobs.py
+++ src/list_jobs.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         'USER_AGENT': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
         # 'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36',
         # 'BOT_NAME': 'Jobs-list-check',
-        'HTTPCACHE_ENABLED': True,
+        # 'HTTPCACHE_ENABLED': True,
         # 'DEFAULT_REQUEST_HEADERS': {
         #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         #   'Accept-Language': 'en'
@@ -32,20 +32,21 @@ if __name__ == '__main__':
         'ITEM_PIPELINES': {
             'higheredjobs_spider.RemovePostdocPipeline': 100,
             'higheredjobs_spider.DeDuplicatesPipeline': 800,
+            'higheredjobs_spider.CsvWriteLatestToOldest': 900,
             },
-        'FEEDS': {
-            Path(RESULT_FILE): {
-                'format': 'csv',
-                'fields': ['posted_date', 'priority_date', 'category',
-                           'school', 'department', 'specialization',
-                           'rank', 'city', 'state', 'canada',
-                           'current_status', 'comments1', 'comments2',
-                           'ads_title', 'ads_source', 'ads_job_code'
-                           ],
-                'overwrite': True,
-                'store_empty': False,
-            },
-        },
+        # 'FEEDS': {
+        #     Path(RESULT_FILE): {
+        #         'format': 'csv',
+        #         'fields': ['posted_date', 'priority_date', 'category',
+        #                    'school', 'department', 'specialization',
+        #                    'rank', 'city', 'state', 'canada',
+        #                    'current_status', 'comments1', 'comments2',
+        #                    'ads_title', 'ads_source', 'ads_job_code'
+        #                    ],
+        #         'overwrite': True,
+        #         'store_empty': False,
+        #     },
+        # },
         'LOG_LEVEL': 'INFO',
         # 'ROBOTSTXT_OBEY': False,
     }
@@ -54,4 +55,4 @@ if __name__ == '__main__':
     process.crawl(JobsHigheredjobsSpider)
     process.start()
 
-    # write_csv_to_google_sheet(RESULT_FILE)
+    write_csv_to_google_sheet(RESULT_FILE)
